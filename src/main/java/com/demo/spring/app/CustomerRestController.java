efb62e748e98aebf30d266d54a870705b696b666
gopisoft26 <gopisoft26@gmail.com>
2021-07-24 15:41:20 +0530

    getMapping added

--- src/main/java/com/demo/spring/app/CustomerRestController.java
+++ src/main/java/com/demo/spring/app/CustomerRestController.java
@@ -1,12 +1,12 @@
 package com.demo.spring.app;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerRestController {
 
-	@RequestMapping(name = "hello")
+	@GetMapping("/hello")
 	public String hello() {
 		return "Hello welcome spring boot rest api.";
 	}
