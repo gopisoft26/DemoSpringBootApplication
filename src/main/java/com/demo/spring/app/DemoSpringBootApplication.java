73234e5fd6340e0b5de4459c7e4a1ad87e776e9e
gopisoft26 <gopisoft26@gmail.com>
2021-07-24 17:09:01 +0530

    servlet message added

--- src/main/java/com/demo/spring/app/DemoSpringBootApplication.java
+++ src/main/java/com/demo/spring/app/DemoSpringBootApplication.java
@@ -2,9 +2,16 @@ package com.demo.spring.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringBootApplication {
+public class DemoSpringBootApplication extends SpringBootServletInitializer {
+  
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoSpringBootApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
